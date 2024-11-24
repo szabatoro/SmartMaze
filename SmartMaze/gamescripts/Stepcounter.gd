94c330ec7ebaeccd361b56dacfe933aa581fb002
szabatoro <szabat21@protonmail.com>
2024-11-24 19:21:05 +0100

    különvett irányú lépések, gombnyomásra láthatóvá váló map pontlevonásért cserébe

--- SmartMaze/gamescripts/Stepcounter.gd
+++ SmartMaze/gamescripts/Stepcounter.gd
@@ -3,4 +3,4 @@ extends Label
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	# maradék lépések kiírása
-	self.text = "Lépések: " + str(get_parent().get_parent().steps)
+	self.text = "LE: " + str(get_parent().get_parent().steps_down) + "\n" + "FEL: " + str(get_parent().get_parent().steps_up) + "\n" + "JOBBRA: " + str(get_parent().get_parent().steps_right) + "\n" + "BALRA: " + str(get_parent().get_parent().steps_left)
