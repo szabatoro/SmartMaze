11c3e9b9852a584040a9b2ce3e382d54f3510d8a
szabatoro <szabat21@protonmail.com>
2024-11-24 19:21:38 +0100

    kezdeti timer átnevezés

--- SmartMaze/gamescripts/FirstTimerLabel.gd
+++
@@ -1,5 +0,0 @@
-extends Label
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	self.text = "Belátási idő: " + str(ceil(get_parent().get_parent().get_child(-4).time_left))
