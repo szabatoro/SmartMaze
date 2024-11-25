ced8736e143a8cc7edb21b638d0cbdae4e677068
Citizen <166248493+Citizen7751@users.noreply.github.com>
2024-11-25 19:56:06 +0100

    Lore beállítva

--- SmartMaze/gamescripts/introtext.gd
+++ SmartMaze/gamescripts/introtext.gd
@@ -12,8 +12,8 @@ func _on_text_timer_timeout():
 
 func _on_next_button_pressed():
 	match current_text:
-		1: update_textbox("[center]Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus tincidunt eros sit amet nunc maximus convallis. Aenean suscipit euismod lacus ut maximus. Ut aliquet mattis leo, vestibulum dignissim mauris rhoncus ut. Aliquam sed congue elit. Phasellus fermentum consequat nibh sed pellentesque. Nunc vehicula mollis suscipit. Nunc luctus lorem ut efficitur. [/center]")
-		2: update_textbox("[center]Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus tincidunt eros sit amet nunc maximus convallis. Aenean suscipit euismod lacus ut maximus. Ut aliquet mattis leo, vestibulum dignissim mauris rhoncus ut. Aliquam sed congue elit. Phasellus fermentum consequat nibh sed pellentesque. Nunc vehicula mollis suscipit. Nunc luctus lorem ut efficitur. [/center]")
+		1: update_textbox("[center]Visszanézel, és gyorsan meggyőződsz arról, hogy nem bírnál visszaindulni, mert mintha mögötted az út észrevétlenül eltűnne, úgy mintha nem is jártál volna ott. \"Nincs más hátra, mint előre\" - jegyzed meg szarkasztikusan, majd a következő lépésnél beszakad alattad a föld. Átesel egy kisebb alagútrendszeren, majd egy tisztáson kötsz ki, ahol egy látszólag ősi kőépület áll. Benézel, és látod hogy nem igazán szabályos az épület belső elrendezése. Egyszercsak egy hangot hallasz onnan bentről.[/center]")
+		2: update_textbox("[center]\"Üdvözlégy utazó! Én, a labirintus beszélek tehozzád. Ez az Örök Tévelygés Erdeje. Innen nem tudsz kijutni, de én kivezethetlek innen. Viszont vannak szabályok! Időben át kell érned az útvesztőimen, vagy az út beszakad alattad és meghalsz, illetve ha túl sokat botorkálsz az utakon, akkor sem fog tudni téged megtartani a föld! De mielőtt elkezded itt utadat, jól nézzd meg merre kell menned, mert nem tart itt örökké a világosság! Emlékezeted menti meg életed! Sok sikert a kiúthoz!\"[/center]")
 		3: get_tree().change_scene_to_file("res://game.tscn")
 
 func update_textbox(update_text):
--- SmartMaze/gamescripts/outrotext.gd
+++ SmartMaze/gamescripts/outrotext.gd
@@ -12,8 +12,8 @@ func _on_text_timer_timeout():
 
 func _on_next_button_pressed():
 	match current_text:
-		1: update_textbox("[center]Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus tincidunt eros sit amet nunc maximus convallis. Aenean suscipit euismod lacus ut maximus. Ut aliquet mattis leo, vestibulum dignissim mauris rhoncus ut. Aliquam sed congue elit. Phasellus fermentum consequat nibh sed pellentesque. Nunc vehicula mollis suscipit. Nunc luctus lorem ut efficitur. [/center]")
-		2: update_textbox("[center]Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus tincidunt eros sit amet nunc maximus convallis. Aenean suscipit euismod lacus ut maximus. Ut aliquet mattis leo, vestibulum dignissim mauris rhoncus ut. Aliquam sed congue elit. Phasellus fermentum consequat nibh sed pellentesque. Nunc vehicula mollis suscipit. Nunc luctus lorem ut efficitur. [/center]")
+		1: update_textbox("[center]\"Szép volt,\" - szólt a labirintus - \"kijutottál, utazó. Mostmár szabad vagy. Melletted az ösvény, ami innen kivisz. \"[/center]")
+		2: update_textbox("[center]Megköszönöd a labirintusnak a segítséget és elindulsz az ösvényen. Nem sokkal később ismerős úton találod magad, ahonnan a pontosan oda érkezel vissza, ahonnan az erdőbe beindultál.\nElsőként jutottál ki az őserdőből.[/center]")
 		3: go_to_scoreboard()
 
 func update_textbox(update_text):
